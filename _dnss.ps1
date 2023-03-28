f4c4b5ed56174d632bb1d2446fccf1bba882d729
Mike Jordan <114234251+MajorDan62@users.noreply.github.com>
2023-03-28 08:24:04 +0100

    Update _dnss.ps1

--- _dnss.ps1
+++ _dnss.ps1
@@ -1,37 +1,39 @@
-######################	
-# RESOLVE DNS HOSTS  #
-# DATED 24-03-2023   #
-# DESIGNER MA JORDAN #
-######################
+########################	
+# RESOLVE DNS HOSTS    #
+# VERSION  20230324_v2 #
+# DATED 24-03-2023     #
+# DESIGNER MA JORDAN   #
+########################
 clear
 $cr="`r`n"
-$_data_file = 	"./datalist.txt" 
+$_data_file 	= 	"./datalist.txt" 
 $_clipflag	=	$True
 $_display 	=	$false
 $_RDNS		=	$false
 $_resolve	=	$false
+$_AbuselPBD	 =	$false
+$_file_		=	$false
 #############
 # FUNCTIONS #
 #############
 function find_domain_ISP($ip)
 {	
-	Invoke-WebRequest -uri "https://www.abuseipdb.com/check/$ip" -outfile "temp.txt"
+	$_url_="https://www.abuseipdb.com/check/$ip" 
+	Invoke-WebRequest -uri $_url_ -outfile "temp.txt"
 	$linenumber		=	( select-string -path temp.txt  -pattern "<th>ISP</th>").linenumber
-	$x				=	($linenumber + 1)
-    $result			=	Get-Content "temp.txt "| Select-Object  -Skip $x  -First 1
+	$x			=	($linenumber + 1)
+    	$result			=	Get-Content "temp.txt "| Select-Object  -Skip $x  -First 1
 
 	$linenumber		=	( select-string -path temp.txt  -pattern "<th>Domain Name</th>").linenumber
-	$x				=	($linenumber + 1)	
+	$x			=	($linenumber + 1)	
 	$domain			=	Get-Content "temp.txt "| Select-Object  -Skip ($x)  -First 1
 
 	$linenumber		=	( select-string -path temp.txt  -pattern "<th>Country</th>").linenumber
-	$x				=	($linenumber + 1)	
-	$country		=	Get-Content "temp.txt "| Select-Object  -Skip ($x+1)  -First 1
-
+	$x			=	($linenumber + 1)	
+	$country		=	Get-Content "temp.txt "| Select-Object  -Skip ($x+1)  -First 
 	$linenumber		=	( select-string -path temp.txt  -pattern "was found in our database!").linenumber
-	if ($linenumber -gt 0 ){$abuse="ABUSE LOGGED"}else{$abuse="NO ABUSE LOGGED"}
-
-    $result			=	$cr + "  ISP    = $result "+ $cr + "  DN     = " + $domain + $cr +  "  LOC    = " + $country +  $cr +  "  STATUS = " + $abuse +$cr
+	if ($linenumber -gt 0 ){if ($_AbuselPBD){[system.Diagnostics.Process]::Start("msedge",$_url_)}$abuse="ABUSE LOGGED"}else{$abuse="NO ABUSE LOGGED"}
+	$result			=	$cr + "  ISP    = $result "+ $cr + "  DN     = " + $domain + $cr +  "  LOC    = " + $country +  $cr +  "  STATUS = " + $abuse +$cr
     return $result
 }
 
@@ -39,12 +41,13 @@ if ($args.count -gt 0)
 {
 	for ($i=0;$i -lt $args.count;$i++)
 	{
-        if ($args[$i] -eq $null)		{$_display		=	$True}
-		if ($args[$i] -eq "-v")			{$_display		=	$True}
-		if ($args[$i] -eq "-noclip")	{$_clipflag		=	$False}
-		if ($args[$i] -eq "-in")		{$_data_file	=	"./"+$args[($i+1)]}
-		if ($args[$i] -eq "-R")			{$_RDNS			=	$True}
-        if ($args[$i] -eq "-P")			{$_resolve		=	$True}
+        if ($args[$i] -eq $null)					{$_display		=	$True}
+		if ($args[$i].ToUpper() -eq "-V")			{$_display		=	$True}
+		if ($args[$i].ToUpper() -eq "-NOCLIP")			{$_clipflag		=	$False}
+		if ($args[$i].ToUpper() -eq "-IN")			{$_data_file	=	"./"+$args[($i+1)];$_file_=$true}
+		if ($args[$i].ToUpper() -eq "-R")			{$_RDNS			=	$True}
+        if ($args[$i].ToUpper() -eq "-P")				{$_resolve		=	$True}
+    	if ($args[$i].Toupper() -eq "-LAUNCH")		 		{$_AbuselPBD	=	$True}
 	}
 }
 else {$_display=$True} 
@@ -53,7 +56,7 @@ if ($_display)
 {
 	write-host "===================================="
 	write-host "| Name    : DNS Name Resolution    |"
-	write-host "| Version : 20230324_v1            |"
+	write-host "| Version : 20230324_v2            |"
 	write-host "| Dated   : 23th March 2023        |"
 	write-host "| Author  : " -nonewline
 	write-host -f green "Mike Jordan" -nonewline
@@ -69,8 +72,14 @@ if ($_display)
 	exit
 }
 $output=""
-$data = Get-Content $_data_file
+$genesis = Get-Date
+
+if ($_file_)
+	{if (Test-Path $_data_file -PathType Leaf){}else{write-host -f white -b red  " **ERROR: NO  DATAFILE LOCATED !! ** $cr    Please retype a valid filename   ";exit}}
+else
+	{write-host -f white -b red " ** ERROR: NO DATAFILE STATED !! ** $CR RUN .\_dnss -in <file> required    " ;exit}
 	
+$data = Get-Content $_data_file	
 if (!$_RDNS)
 {
 	write-host -b green -f white "-- DNS LOOKUP --"
@@ -87,7 +96,7 @@ if (!$_RDNS)
 			catch 
 			{
 				if ($line[0] -gt $null)
-				{Write-Host -b red -f black " '$line' DNS Lookup Failed "}
+				{Write-Host -b red -f black " '$line' DNS Lookup Failed $cr"f}
 				$failed=$True
 			}
 			
@@ -99,9 +108,12 @@ if (!$_RDNS)
 					$_output=""
 					do
 						{
-							$output= $result[$count].name + " - " + $result[$count].ipaddress + " **DUPLICATE**"
-							write-host $output
-							$_output= $_output+ $result[$count].name + " - " + $result[$count].ipaddress + " **DUPLICATE**"+$cr
+							if (($result[$count].ipaddress) -like "*.*" )
+							{
+								$output= $result[$count].name + " - " + $result[$count].ipaddress + " **DUPLICATE**"
+								write-host $output
+								$_output= $_output+ $result[$count].name + " - " + $result[$count].ipaddress + " **DUPLICATE**"+$cr
+							}
 							$count=$count + 1
 						}	
 					while ($result.count -gt $count )
@@ -109,8 +121,11 @@ if (!$_RDNS)
 					}
 					else
 						{
-							$output= $result.name + " - " + $result.ipaddress
-							write-host $output
+							if (($result[$count].ipaddress) -like "*.*" )
+							{
+								$output= $result.name + " - " + $result.ipaddress
+								write-host $output
+							}
 						}
 					$summary=$summary+$output+$cr
 					}
@@ -151,7 +166,7 @@ if (!$_RDNS)
 				catch 
 				{
 					if ($line[0] -gt $null)
-					{Write-Host -b red -f black " '$line' Reserve DNS Lookup Failed "}
+					{Write-Host -b red -f black " '$line' Reverse DNS Lookup Failed $cr"}
 					$result=$line
 					$failed=$True
 				}
@@ -189,7 +204,7 @@ if (!$_RDNS)
 									$_rv_		=	find_domain_ISP($_ip_)
 									$output		= 	 $output +  $_rv_
 							}
-							write-host $output 
+							write-hoAst $output 
 						}
 						$summary=$summary+$output+$cr
 					}
@@ -197,4 +212,8 @@ if (!$_RDNS)
 			}
 		if ($_clipflag){$summary  -replace "`r`n`r`n", "`r`n"| sort | clip}else{Set-Clipboard -Value $null}
 		}
+	write-host
+	$exodus=Get-Date
+	$delta=$exodus-$genesis;$h="{0:D2}" -f [int]$delta.hours;$m="{0:D2}" -f [int]$delta.minutes;$s="{0:D2}" -f [int]$delta.seconds
+	write-host -b blue -f white " [_DNSS] Script Active for"$h"h "$m"m "$s"s ";
 	write-host
